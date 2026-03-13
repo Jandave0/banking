010bd0a205280801b963889a9a1c7404510e60c5
Jandave0 <185737041+Jandave0@users.noreply.github.com>
2026-03-13 17:07:32 +0800

    feat: initial commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
-import layout from '../layout'
+import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+    const loggedIn = {firstName: 'Babid'};
   return (
-    <div>Home</div>
+    <section className='home'>
+        <div className='home-content'>
+        <header className='home-header'>  
+        <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="What would you like to do today?"
+        />
+       <TotalBalanceBox 
+        accounts={[]} 
+        totalBanks={5} 
+        totalCurrentBalance={12345.67} 
+       />
+       </header>
+        </div>
+    </section>
   )
 }
 
